b20c9daa5b76af50100250c123bd3bef6dab64bd
Tamsi Besson <tamsi.besson@gmail.com>
2017-08-20 17:37:30 +0200

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+  return(
+    [
+      {title: 'Js: The Good Parts', pages: 101},
+      {title: 'Harry Potter', pages: 39},
+      {title: 'The Dark Tower', pages: 506},
+      {title: 'Eloquent Ruby', pages: 1}
+    ]
+  );
+}
